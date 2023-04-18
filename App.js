f0605eae04c0b6d22d5b95b1e1c8c48806f7fd15
Alets30 <ettosn30@gmail.com>
2023-04-17 23:52:41 -0600

    Estructura del login

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloud } from '@fortawesome/free-solid-svg-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>¡Hola mundo!</Text>
+      <FontAwesomeIcon icon={faCloud} size="7x" style={{color: "#5398fb",} }/>
+      <Text>¡Bienvenido a mi aplicación!</Text>
+      <TextInput style={{width: "90%"}} placeholder="Correo electrónico"/>
+      <TextInput style={{width: "90%"}} placeholder="Contraseña"/>
       <StatusBar style="auto" />
     </View>
   );
