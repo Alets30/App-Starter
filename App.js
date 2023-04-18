988823468d1476f94efe2ac2d24b98e50291befc
Alets30 <ettosn30@gmail.com>
2023-04-18 00:19:48 -0600

    Se agregó el botón de iniciar sesión

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,6 +12,9 @@ export default function App() {
       <Text style={{marginBottom: "10px"}}>¡Bienvenido a mi aplicación!</Text>
       <TextInput style={{width: "90%", padding: "10px", marginBottom: "10px", borderColor: "#bee8dd", borderWidth: 2, borderRadius: 5}} placeholder="Correo electrónico"/>
       <TextInput style={{width: "90%", padding: "10px", marginBottom: "10px", borderColor: "#bee8dd", borderWidth: 2, borderRadius: 5}} placeholder="Contraseña"/>
+      <button style={{width: "90%", background: '#162749'}} href="#">
+      <Text style={{color: 'white'}}>Iniciar sesión</Text>
+      </button>
       <StatusBar style="auto" />
     </View>
   );
