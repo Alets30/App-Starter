efa8966aa0ea9cc0e3bc6b6896656bfb20b5ed93
Alets30 <ettosn30@gmail.com>
2023-04-18 20:34:40 -0600

    Estructurando el diseño del login

--- App.js
+++ App.js
@@ -1,30 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloud } from '@fortawesome/free-solid-svg-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <FontAwesomeIcon icon={faCloud} size="10x" style={{color: "#5398fb", marginBottom: "10px"} }/>
-      <Text style={{marginBottom: "10px", fontSize: '25px'}}>¡Bienvenido a mi aplicación!</Text>
-      <TextInput style={{width: "90%", padding: "10px", marginBottom: "10px", borderColor: "#bee8dd", borderWidth: 2, borderRadius: 5, fontSize: '15px'}} placeholder="Correo electrónico"/>
-      <TextInput style={{width: "90%", padding: "10px", marginBottom: "10px", borderColor: "#bee8dd", borderWidth: 2, borderRadius: 5, fontSize: '15px'}} placeholder="Contraseña"/>
-      <button style={{width: "90%", background: '#162749', padding: '10px'}} href="#">
-      <Text style={{color: 'white', fontSize: '20px'}}>Iniciar sesión</Text>
-      </button>
-      <StatusBar style="auto" />
+    <View style={estilos.contenedor}>
+      <View style={estilos.formulario}>
+        <Text style={estilos.titulo}>¡Bienvenido a mi aplicación!</Text>
+        <TextInput style={estilos.input} placeholder="Correo electrónico"/>
+        <TextInput style={estilos.input} placeholder="Contraseña"/>
+        <TouchableOpacity style={estilos.boton}>
+          <Text style={estilos.texto}>Iniciar sesión</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const estilos = StyleSheet.create({
+  contenedor: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  formulario: {
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  titulo: {
+  marginBottom: "10px", 
+  fontSize: '25px'
+  },
+  input: {
+    width: "100%", 
+    padding: "10px", 
+    marginBottom: "10px", 
+    borderColor: "#bee8dd", 
+    borderWidth: 2, 
+    borderRadius: 5, 
+    fontSize: '15px'
+  },
+  boton: {
+    width: '100%',
+    textAlign: 'center',
+    padding: '10px',
+    fontSize: '20px',
+    borderWidth: 3,
+    borderRadius: 5
+  },
+  texto: {
+    fontSize: '20px',
+  }
 });
